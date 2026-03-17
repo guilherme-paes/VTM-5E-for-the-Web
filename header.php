d5d997be0989ececd577374859b8ad85892cc09e
Guilherme Paes <guilhermeaugustopaes_eu@hotmail.com>
2026-03-16 21:08:44 -0300

    commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+
+<head>
+
+
+</head>
+
+
+
+<body>
+
+    <nav>
+        <ul>
+            <li> <a href='./login.php'></li>
+            <li> <a href='./editor'> </li>
+            <li> <a href='./editor'> </li>
+            <li> <a href='./clans'> </li>
+            <li> <a href='./disciplines'> </li>
+        </ul>
+        
+    </nav>
